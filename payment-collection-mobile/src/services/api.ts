1cf0dba3a5b096d2255d36e0bef082ba73ab516e
vyshakh <vyshakhck98@gmail.com>
2026-09-16 14:27:27 +0530

    Connect mobile app to deployed API

--- payment-collection-mobile/src/services/api.ts
+++ payment-collection-mobile/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://192.168.1.6:5000';
+const API_BASE_URL = 'http://13.235.105.99:5000';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
